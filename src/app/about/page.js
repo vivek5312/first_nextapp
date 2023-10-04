ff31c75ae626903a1ee1aa29a0f79157a2e7b134
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-10-04 22:22:26 +0530

    dynamic route

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,23 @@
-const About =()=>{
-    return <h1>My About Page</h1>
-}
+import Link from "next/link";
+
+const About = () => {
+ const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+
+  return (
+    <div>
+      <h1>My About Page</h1>
+      {details.map((detail) => (
+        <div key={detail.id}>
+           <Link href={`/about/${detail.id}`} >
+          <p>{detail.name} {detail.role}</p>
+          </Link>
+        </div> 
+      ))}
+    </div>
+  );
+};
 export default About;
